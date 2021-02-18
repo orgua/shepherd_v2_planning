1c0545af0c4a949f8df0ebd092ea3ecd92be657e
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-02-18 11:25:46 +0100

    Create test_clock_correction_distribution.py

---
+++ timesync/test_clock_correction_distribution.py
@@ -0,0 +1,29 @@
+import copy
+
+buffer_block_period = 20001333
+analog_sample_period = 2000
+analog_sample_steps = 10000
+compensation_steps = 1333
+compensation_distance = 7
+
+analog_sample_index = 0
+compensation_dist_count = 0
+
+for index in range(analog_sample_steps):
+
+    value = copy.deepcopy(analog_sample_index)
+    value = value + analog_sample_period
+    compensation_dist_count += 1
+
+    if (compensation_dist_count >= compensation_distance) and (compensation_steps > 0):
+        value += 1
+        compensation_steps -= 1
+        compensation_dist_count = 0
+
+    if value > buffer_block_period:
+        print(f"Value={value}, bigger than {buffer_block_period}")
+        value = buffer_block_period
+
+    analog_sample_index = value
+
+print(f"Final value = {analog_sample_index}, expected = {buffer_block_period}")
